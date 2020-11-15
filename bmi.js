cb27148a8f4e758c675a1b1dd760ba2e4e47bae9
Chris <chansonart84@gmail.com>
2020-11-15 16:08:28 -0600

    Updated JS

--- bmi.js
+++ bmi.js
@@ -2,8 +2,24 @@ $(document).ready(
     function () {
 
 //add event handlers
-
+var myRules {
+    weight:{required:true
+    min: 88
+    max: 353
+    digits: true},
+            height:{required:true
+    min: 59
+    max: 79
+    digits:true},
+        }
+        var myMessages = {
+    weight: {required:"Weight is required"},
+            height: {required:"Height is required"}
+        };
         //all other functions (program logic)
+        $("form").validate({submitHandler: calcBmi,
+        rules:myRules,
+        messages:myMessages});
 
     }
 )
